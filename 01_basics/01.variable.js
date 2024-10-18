80b365f0134126b59b02150f84c878b39ad30ebf
Ayan Gorain <100009409+ayan-gorain@users.noreply.github.com>
2024-10-18 15:47:47 +0530

    2nd commit

---
+++ 01_basics/01.variable.js
@@ -0,0 +1,13 @@
+const accountId=121334
+let accountEmail="ayan@gmail.com"
+var accountPass="12455"
+accountcity="jaipur"
+let accoountstate;
+
+// AccoutId=2 //not allowed
+accountEmail="hc@gmail.com"
+accountPass="225226526526"
+accountcity="jaipur"
+
+
+console.table([accountId,accountEmail,accountPass,accountcity,accoountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";// treat all js code as newer verion
+
+//alert("hello") we are using node js and browser
+
+
+console.log( 9+8) 
+
+
+
+let name="hitesh"
+let age=18
+let isloggedin=false
+
+
+// number => 2 to the power 53
+//bigint
+//string => " "
+//boolean =>true// false
+//undefined =>
+
+
