c8fdc91a37a20acaf55ac5e4130f6336369ef680
Ayan Gorain <100009409+ayan-gorain@users.noreply.github.com>
2024-10-19 14:28:02 +0530

    gg

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,17 @@
+// let score=null
+
+
+// console.log(typeof score);
+
+
+// let valueinno=Number(score)
+
+
+// console.log(typeof valueinno);
+// console.log( valueinno);
+
+let isloggedin=hitesh
+
+let booleanIsLogged =Boolean(isloggedin)
+
+console.log(booleanIsLogged)
